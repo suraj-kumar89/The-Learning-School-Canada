db0a0581ae63605c9aacf2aa861156b151736a03
suraj-kumar89 <suraj@bluvodigital.com>
2026-03-04 00:47:37 +0530

    update content

--- app/tef-trial-clear-tef-in-120-days/page.tsx
+++ app/tef-trial-clear-tef-in-120-days/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import Form from "@/SharedComponent/Form";
 import Header from "@/SharedComponent/Header";
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-[#F5F4F6] w-full">
-      <Header/>
-      <Form />
+      <Header />
+      <Suspense fallback={<div />}>
+        <Form />
+      </Suspense>
     </main>
   );
 }
